793cebdaef7c1d6d8a01ebd87e7bd968ea562b67
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-05-06 20:47:07 +0530

    Navbar improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,17 +8,14 @@ export const Navbar = () => {
             <Flex h={14} bg={'#F5F5F5'}>
                 <Center>
                     <Link to={'/'}>
-                        <Heading ml={5} fontSize={20}>ProKart</Heading>
+                        <Heading ml={5} fontSize={['16px','20px']}>Mini Store</Heading>
                     </Link>
                 </Center>
                 <Spacer />
                 <Center>
-                    <Button mr={4} ><Link to={"/"}>Home</Link></Button>
-                    <Button mr={4} ><Link to={"/products"}>Products</Link></Button>
-                    <Button mr={4} ><Link to={"/about"}>About</Link></Button>
-                    <Button mr={4} ><Link to={"/contact"}>Contact us</Link></Button>
-                    <Button mr={4} ><Link to={"/faq"}>FAQ</Link></Button>
-                    <Button mr={4} ><Link to={"/"}>Login</Link></Button>
+                    <Button bg={'transparent'} mr={'2px'} ><Link to={"/"}>Home</Link></Button>
+                    <Button bg={'transparent'} mr={'2px'} ><Link to={"/products"}>Products</Link></Button>
+                    <Button bg={'transparent'} mr={'2px'} ><Link to={"/"}>Login</Link></Button>
                 </Center>
             </Flex>
         </>
