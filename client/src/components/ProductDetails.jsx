c070eb77f17af1188edce584b254438ced424fcc
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-05-07 18:51:11 +0530

    product description page improved

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { Box, Center, HStack, Button, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, Image, Text, Flex, Grid } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -12,7 +12,7 @@ export const ProductDetails = () => {
     const [num, setNum] = useState(0);
     const dispatch = useDispatch();
     const { isLoading, oneProduct, isError } = useSelector((state) => state.oneProductReducer, shallowEqual);
-    const {img, name, category, gender, size, brand, rating, collections, price } = oneProduct;
+    const { img, name, category, gender, size, brand, rating, collections, price } = oneProduct;
 
     const handleImageChange = (value) => setNum(num + value);
 
@@ -20,37 +20,37 @@ export const ProductDetails = () => {
         dispatch(getOneDataRequest(id));
     }, [id]);
 
+
     return isLoading ? (
         <Loading />
     ) : isError ? (
         <Error />
     ) : (
-        <>
-            <SimpleGrid columns={2} m={"20px auto"} w={1100} >
-                <Box w={500} boxShadow='lg' overflow={'hidden'}>
-                    <Image src={img[num]} />
+        <Grid m={'40px auto'} p={'10px'} gap={'10px'} maxW={'1100px'} templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)']} >
 
+            <Box>
+                <Box borderRadius={'10%'} boxShadow='lg' overflow={'hidden'}>
+                    <Image className="zoom" src={img[num]} />
                 </Box>
-                <Box overflow={'hidden'} p={10} >
-                    <Text my={2} fontSize={18} color={'grey'}  >{category}</Text>
-                    <Text my={2} fontSize={30}><b>{name}</b></Text>
-                    <Text my={4} fontSize={40} color={'red'}>
-                        <Text color={'grey'} as='del'>₹{price + Math.floor(0.2 * price)}.00</Text> &nbsp;
-                        ₹{price}.00
-                    </Text>
-                    <Text my={2} fontSize={20} color={'grey'}>Brand: {brand}</Text>
-                    <Text my={2} fontSize={20} color={'grey'}>Collection: {collections}</Text>
-                    <Text my={2} fontSize={20} color={'grey'}>Rating: {rating}</Text>
-                    <Text my={2} fontSize={20} color={'grey'}>Sizes: {size.join(", ")}</Text>
-                    <Text my={2} fontSize={20} color={'grey'}>Gender: {gender === 'men' ? 'Men' : 'Women'}</Text>
-                </Box>
-                <Center>
-                    <HStack mt={5} gap={3}>
-                        <Button onClick={() => { handleImageChange(-1) }} disabled={num === 0} >Prev</Button>
-                        <Button onClick={() => { handleImageChange(1) }} disabled={num === (img.length - 1)}>Next</Button>
-                    </HStack>
-                </Center>
-            </SimpleGrid>
-        </>
+                <Flex justifyContent={'center'} gap={'30px'} my={'20px'}>
+                    <Button onClick={() => { handleImageChange(-1) }} disabled={num === 0} >Prev</Button>
+                    <Button onClick={() => { handleImageChange(1) }} disabled={num === (img.length - 1)}>Next</Button>
+                </Flex>
+            </Box>
+
+            <Box overflow={'hidden'} p={'30px'} >
+                <Text my={2} fontSize={18} color={'grey'}  >{category}</Text>
+                <Text my={2} fontSize={['22px', '30px']}><b>{name}</b></Text>
+                <Text my={4} fontSize={['30px', '40px']} color={'red'}>
+                    <Text color={'grey'} as='del'>₹{price + Math.floor(0.2 * price)}.00</Text> &nbsp;
+                    ₹{price}.00
+                </Text>
+                <Text my={2} fontSize={20} color={'grey'}>Brand: {brand}</Text>
+                <Text my={2} fontSize={20} color={'grey'}>Collection: {collections}</Text>
+                <Text my={2} fontSize={20} color={'grey'}>Rating: {rating}</Text>
+                <Text my={2} fontSize={20} color={'grey'}>Sizes: {size.join(", ")}</Text>
+                <Text my={2} fontSize={20} color={'grey'}>Gender: {gender === 'men' ? 'Men' : 'Women'}</Text>
+            </Box>
+        </Grid>
     );
 };
