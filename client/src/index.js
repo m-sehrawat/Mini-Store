1b4ddff04e40bde24e5c9e5e6a05d75193e0502f
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-05-07 15:42:57 +0530

    redux provider added

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './redux/store';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <Router>
-        <App />
-      </Router>
+      <ReduxProvider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </ReduxProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
