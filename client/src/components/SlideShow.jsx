1712fcd97218451f8e61f8b76b5292a9feaf93e8
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-05-09 21:57:54 +0530

    slide show added on home page

---
+++ client/src/components/SlideShow.jsx
@@ -0,0 +1,31 @@
+import { Box, Center, Flex, Icon } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { AiFillCaretRight, AiFillCaretLeft } from "react-icons/ai";
+
+export const SlideShow = () => {
+
+    const [index, setIndex] = useState(0);
+
+    const handleIndexChange = (value) => {
+        setIndex((prev) => prev === 5 ? 0 : prev === 0 ? 4 : prev + value);
+    }
+
+    useEffect(() => {
+        setInterval(() => {
+            setIndex((prev) => prev === 5 ? 0 : prev + 1);
+        }, 6000);
+    }, []);
+
+    return (
+        <>
+            <Box className="slide" backgroundImage={`slideImages/img${index}.jpg`} backgroundRepeat={'no-repeat'} backgroundSize={'cover'} borderRadius={'30px'} h={['150px', '200px', '300px', '400px']} shadow={'lg'}>
+                <Center px={'15px'} h={['150px', '200px', '300px', '400px']}>
+                    <Flex w={'100%'} justifyContent={'space-between'}>
+                        <Icon onClick={() => { handleIndexChange(-1) }} className="expand" w={['30px', '40px', '50px']} h={['30px', '40px', '50px']} _hover={{ 'bg': 'white' }} padding={['5px', '7px', '10px']} borderRadius={'50%'} as={AiFillCaretLeft} />
+                        <Icon onClick={() => { handleIndexChange(1) }} className="expand" w={['30px', '40px', '50px']} h={['30px', '40px', '50px']} _hover={{ 'bg': 'white' }} padding={['5px', '7px', '10px']} borderRadius={'50%'} as={AiFillCaretRight} />
+                    </Flex>
+                </Center>
+            </Box>
+        </>
+    );
+};
